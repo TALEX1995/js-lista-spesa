e4c6bbd1d1362c1614a89be267093327462d5914
TALEX1995 <tanase.alex@libero.it>
2023-06-05 15:17:10 +0200

    Added exercise with for

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
 console.log('JS Ok')
+
+// Recover DOM Element
+
+shoppingListPlaceholder = document.getElementById('shopping-list');
+
+
+// Create Array
+
+shoppingListArray = ['uova', 'pane', 'pollo', 'pizza', 'redbull', 'patate', 'mozzarella'];
+
+
+// Create shopping list
+shoppingList = ''
+
+for (let i = 0; i < shoppingListArray.length; i++) {
+    shoppingList += `<p>${shoppingListArray[i]}</p>`;
+}
+
+shoppingListPlaceholder.innerHTML = shoppingList
