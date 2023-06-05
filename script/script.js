f453516a2fded7a2a5fb8e6a4b47afd9b2066960
TALEX1995 <tanase.alex@libero.it>
2023-06-05 15:21:50 +0200

    Added cicle with while

--- script/script.js
+++ script/script.js
@@ -13,8 +13,27 @@ shoppingListArray = ['uova', 'pane', 'pollo', 'pizza', 'redbull', 'patate', 'moz
 // Create shopping list
 shoppingList = ''
 
-for (let i = 0; i < shoppingListArray.length; i++) {
-    shoppingList += `<p>${shoppingListArray[i]}</p>`;
+// Crate variable index
+
+currentIndex = 0
+
+// Cicle with while
+
+while (currentIndex !== shoppingListArray.length) {
+
+    // Create Node
+    shoppingList += `<p>${shoppingListArray[currentIndex]}</p>`;
+
+    // Increment current Index
+    currentIndex++
 }
+
+
+
+
+// Cicle with for
+// for (let i = 0; i < shoppingListArray.length; i++) {
+//     shoppingList += `<p>${shoppingListArray[i]}</p>`;
+// }
 
 shoppingListPlaceholder.innerHTML = shoppingList
